33f7340adb0cd9927384050d62146a029a5f8179
NinoMartkoplishvili <23202154@ibsu.edu.ge>
2024-11-01 19:52:30 +0400

    first commit5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+loadApiData() {
+throw new Error('Method not implemented.');
+}
+onSubmit() {
+throw new Error('Method not implemented.');
+}
   title = 'MN5';
+  constructor(private fb: FormBuilder) {}
+
+userForm = this.fb.group({
+  firstname: ['', [Validators.required, Validators.minLength(3)]],
+  lastname: ['', [Validators.required, Validators.minLength(3)]],
+  phoneNumber: ['', [Validators.required, Validators.minLength(9)]],
+  email: ['', [Validators.required, Validators.minLength(8)]]
+});
 }
---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number;
+    firstname: string;
+    lastname: string;
+    dateOfBirth: string;
+    phoneNumber: string;
+    email: string;
+    
+}
